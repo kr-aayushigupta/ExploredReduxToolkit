dcc721bb649f4a330ff29a7cc4af44fb74bf2b99
kr-aayushigupta <aayushi.g@knackroot.com>
2025-03-06 16:42:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useSelector,useDispatch} from 'react-redux';
 import './App.css';
-
+import './mycss.css';
+import {actions} from "./store/index"
+// useselctor is a custom of react-redux that lets your functional components to read data from store : here data from store is state 
 function App() {
+    const counter=useSelector((state)=>state.counter)
+    const dispatch=useDispatch();
+    
+    const increment=()=>{
+      dispatch(actions.increment());
+    }
+
+    const decrement=()=>{
+      dispatch(actions.decrement());
+      
+    }
+
+    const addby=()=>{
+      dispatch(actions.addby(10));
+      
+    }
+    const subby=()=>{
+      dispatch(actions.subby(10));
+      
+    }
+    const reset=()=>{
+      dispatch(actions.reset(0));
+      
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 className='div1'>Counter App</h1>
+      <h3 className='div2'>Using REDUX TOOLKIT in React app</h3>
+      <h2 className='div1'>Counter : {counter}</h2>
+      <div className='mydiv'>
+      <button  onClick={increment}>Increment</button>
+      
+      <button onClick={decrement}>Decrement</button>
+      <button  onClick={addby}>Increment by 10</button>
+      <button  onClick={subby}>Decrement by 10</button>
+      <button  onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import {configureStore,configureSlice, createSlice} from '@reduxjs/toolkit';
+
+const counterSlice=createSlice({
+
+    name: 'counter',
+    initialState:{counter:0},
+    reducers:{
+        increment(state,action){
+            state.counter++;
+        },
+        decrement(state,action){
+            state.counter--;
+        },
+        addby(state,action){
+            state.counter+=action.payload;
+        },
+        subby(state,action){
+            state.counter-=action.payload;
+        },
+        reset(state,action){
+            state.counter=action.payload;
+        },
+    }
+});
+export const actions = counterSlice.actions;
+const store=configureStore ({
+    reducer: counterSlice.reducer
+})
+
+export default store;
